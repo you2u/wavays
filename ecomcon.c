c97fc5a8a02bf346b021cbaae131b080a5377a68
you2u <118322983+you2u@users.noreply.github.com>
2022-12-04 06:42:24 +0000

    drm/radeon: disable tests/benchmarks if accel is disabled

--- ecomcon.c
+++ ecomcon.c
@@ -6,3 +6,9 @@
     Public Domain
 
     Ecomcon is a simple preprocessor. It enables comments conditionally. The
+    comments can be enabled by supplying tag names on the command line. These
+    comments are normally ignored, and will be removed by JSMin. But ecomcon
+    will activate these comments, making them executable. They can be used for
+    development time activities like debugging, testing, logging, or tracing. A
+    conditional comment is a '//' style comment that starts at the left margin.
+    The '//' is immediately followed by a <tag>. There must be no space between
