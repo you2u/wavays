ede408296cf93c04e52bc222b4872e644bace5aa
you2u <118322983+you2u@users.noreply.github.com>
2023-02-25 19:29:34 +0000

    boom

--- ecomcon.js
+++ ecomcon.js
@@ -6,3 +6,9 @@
 const rx_crlf = /\n|\r\n?/;
 
 const rx_ecomcon = /^\/\/([a-zA-Z0-9_]+)\u0020?(.*)$/;
+
+//. Capturing groups:
+//.  [1] The enabled comment tag
+//.  [2] The rest of the line
+
+const rx_tag = /^[a-zA-Z0-9_]+$/;
